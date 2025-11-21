0faec521cc50207ce7c68022baa3bc2ccc5a5247
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-21 15:11:14 +0100

    adapted client.model.js to use newly suggested structure

--- model/client.model.js
+++ model/client.model.js
@@ -15,13 +15,48 @@ const clientModel = {
         }
     },
 
-    selectClient: async (client) => {
+    selectClient: async (filters) => {
         let con;
         try {
             con = await db.connectToDB()
-            const searchPattern = `%${client}%`;
-            const rows = await con.query('SELECT * FROM clients WHERE id LIKE ?', [searchPattern]);
-            return rows[0];
+            let sql = "SELECT * FROM clients WHERE 1=1";
+            let params = [];
+
+            if (filters.id) {
+                sql += " AND id = ?";
+                params.push(`%${filters.id}%`);
+            }
+            if (filters.lastname) {
+                sql += " AND lastname LIKE ?";
+                params.push(filters.lastname);
+            }
+            if (filters.firstname) {
+                sql += " AND firstname LIKE ?";
+                params.push(filters.firstname);
+            }
+            if (filters.genre) {
+                sql += " AND genre = ?";
+                params.push(filters.genre);
+            }
+            if (filters.email) {
+                sql += " AND email LIKE ?";
+                params.push(filters.email);
+            }
+            if (filters.phone_number) {
+                sql += " AND phone_number LIKE ?";
+                params.push(filters.phone_number);
+            }
+            if (filters.address) {
+                sql += " AND address LIKE ?";
+                params.push(filters.address);
+            }
+
+            const [rows] = await con.query(sql, params);
+            return rows;
+            // const searchPattern = `%${client}%`;
+            // const rows = await con.query('SELECT * FROM clients WHERE id LIKE ?', [searchPattern]);
+            // console.log(searchPattern)
+            // return rows[0];
         } catch (error) {
             console.log("Error fetching client:", error);
             throw error;
