a01d7a108e8a336928bce90061057b6bfab6a964
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-21 15:11:21 +0100

    adapted client.route.js to use newly suggested structure

--- routes/client.route.js
+++ routes/client.route.js
@@ -4,16 +4,21 @@ import clientService from "../service/client.service.js";
 const router = express.Router();
 router.use(express.json());
 
-// router.get('/', (req, res) => {
-//     res.send('Hello Locality!')
-// })
-
 router.get('/', async (req, res) => {
-    if (req.query.client) {
+    if (req.query) {
         try {
-            let client = await clientService.getClient(req.query.client);
-            let message = `Le client ${req.query.client} a bien été récupérée !`;
-            res.json({message: message, body: client});
+            let client = await clientService.getClient({
+                id: req.query.id,
+                lastname: req.query.lastname,
+                firstname: req.query.firstname,
+                genre: req.query.genre,
+                email: req.query.email,
+                phone_number: req.query.phone_number,
+                address: req.query.address
+            });
+            //let message = `Le client ${req.query.client} a bien été récupérée !`;
+            //res.json({message: message, body: client});
+            res.json({ body: client});
         } catch (error) {
             res.status(404).json({error: error.message});
         }
