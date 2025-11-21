f90f8910379b2806ba96db02cbc6fd3df141e792
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-21 15:11:28 +0100

    adapted client.service.js to use newly suggested structure

--- service/client.service.js
+++ service/client.service.js
@@ -1,9 +1,25 @@
 import clientModel from '../model/client.model.js';
 
 const clientService = {
-    getClient: async (client) => {
+    getClient: async (filters) => {
+        const query = {};
+        if (filters.id) query.id = filters.id;
+        if (filters.lastname) query.lastname = filters.lastname;
+        if (filters.firstname) query.firstname = filters.firstname;
+        if (filters.genre) query.genre = filters.genre;
+        if (filters.email) query.email = filters.email;
+        if (filters.phone_number) query.phone_number = filters.phone_number;
+        if (filters.address) query.address = filters.address;
+
+        // try {
+        //     client = await clientModel.selectClient(client);
+        //     return client;
+        // } catch (error) {
+        //     console.log("Error fetching client[service]:", error);
+        // }
+
         try {
-            client = await clientModel.selectClient(client);
+            let client = await clientModel.selectClient(query);
             return client;
         } catch (error) {
             console.log("Error fetching client[service]:", error);
