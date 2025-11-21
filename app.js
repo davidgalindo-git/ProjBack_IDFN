9c5f11cf77f8708b3d55fea17b7c07d8858cc33e
NathanFilipowitz <py58wpb@eduvaud.ch>
2025-11-21 13:55:09 +0100

    added route for clients

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ import express from 'express'
 const app = express()
 const port = process.env.PORT
 import localityRouter from './routes/locality.route.js';
+import clientRouter from './routes/client.route.js';
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!!!')
 })
 
 // Locality route
 app.use('/locality', localityRouter);
+
+// Client route
+app.use('/client', clientRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
