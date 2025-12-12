460b1007e60fa9e69dd5f5186a558ca465096a3f
Fabian <rostellofabian@gmail.com>
2025-12-12 14:37:00 +0100

    feat: added complete CRUD for locality

--- model/locality.model.js
+++ model/locality.model.js
@@ -1,7 +1,7 @@
 import { db } from '../db/db.js'
 
 const localityModel = {
-    selectLocality: async (filters) => {
+    getLocality: async (filters) => {
         let con;
         try {
             con = await db.connectToDB();
@@ -29,23 +29,102 @@ const localityModel = {
                 sql += " AND canton_code = ?";
                 params.push(filters.canton_code);
             }
-            if (filters.lang_code) {
+            if (filters.language_code) {
                 sql += " AND language_code = ?";
-                params.push(filters.lang_code);
+                params.push(filters.language_code);
             }
 
             const [rows] = await con.query(sql, params);
-            return rows [0];
+            return rows;
         } catch (error) {
             throw SQLException;
         } finally {
             await db.disconnectToDB(con);
         }
     },
-}
+    postLocality: async (filters) => {
+        let con;
+        try {
+            con = await db.connectToDB();
+            const rows = await con.query('INSERT INTO locations (name, postal_code, postal_code_complement, toponym, canton_code, language_code) VALUES (?, ?, ?, ?, ?, ?)', [filters.name, filters.postal_code, filters.postal_code_complement, filters.toponym, filters.canton_code, filters.language_code]);
+            return rows[0];
+        } catch (error) {
+            throw SQLException;
+        } finally {
+            await db.disconnectToDB(con);
+        }
+    },
+    patchLocality: async (filters) => {
+        let con;
+        try {
+            con = await db.connectToDB();
 
-const localityHelpers = {
+            let sql = "UPDATE locations SET";
+            let params = [];
 
+            if (filters.name !== null) {
+                sql += " name = ?";
+                params.push(filters.name);
+            }
+            if (filters.postal_code !== null) {
+                sql += ", postal_code = ?";
+                params.push(filters.postal_code);
+            }
+            if (filters.postal_code_complement !== null) {
+                sql += ", postal_code_complement = ?";
+                params.push(filters.postal_code_complement);
+            }
+            if (filters.toponym !== null) {
+                sql += ", toponym = ?";
+                params.push(filters.toponym);
+            }
+            if (filters.canton_code !== null) {
+                sql += ", canton_code = ?";
+                params.push(filters.canton_code);
+            }
+            if (filters.language_code !== null) {
+                sql += ", language_code = ?";
+                params.push(filters.language_code);
+            }
+            sql += " WHERE id = ?;"
+            params.push(filters.id);
+
+            const [rows] = await con.query(sql, params);
+            return rows.affectedRows;
+        } catch (error) {
+            throw SQLException;
+        } finally {
+            await db.disconnectToDB(con);
+        }
+    },
+    deleteLocality: async (id) => {
+        let con;
+        try {
+            con = await db.connectToDB();
+            const [rows_link] = await con.query('DELETE FROM services WHERE location_id = ?', id);
+
+            if (rows_link.affectedRows >= 1){
+                const [rows] = await con.query('DELETE FROM locations WHERE id = ?', id);
+                return rows.affectedRows;
+            }
+        } catch (error) {
+            throw SQLException;
+        } finally {
+            await db.disconnectToDB(con);
+        }
+    },
+    isIdValid: async (id) => {
+        let con;
+        try {
+            con = await db.connectToDB();
+            const rows = await con.query('SELECT * FROM locations WHERE id = ?', [id]);
+            return rows[0].length > 0;
+        } catch (error) {
+            throw SQLException;
+        } finally {
+            await db.disconnectToDB(con);
+        }
+    }
 }
 
 export default localityModel;
--- routes/locality.route.js
+++ routes/locality.route.js
@@ -7,24 +7,109 @@ router.use(express.json());
 router.get('/', async (req, res) => {
     try {
         let message = "";
+        const {name, postal_code, postal_code_complement, toponym, canton_code, language_code} = req.body || {};
+
         let locality = await localityService.getLocality({
-            name: req.query.name,
-            postal_code: req.query.postal_code,
-            postal_code_complement: req.query.postal_code_complement,
-            toponym: req.query.toponym,
-            canton_code: req.query.canton_code,
-            lang_code: req.query.lang_code
+            name,
+            postal_code,
+            postal_code_complement,
+            toponym,
+            canton_code,
+            language_code
         });
 
-        if (locality.length !== 0) {
-            message = `La localité ${locality["name"]} à bien été trouvée`;
-        } else {
+        if (locality === null || locality === undefined) {
             message = `Aucune localité n'a été trouvé`;
+            res.status(404).json({error: message});
+        } else {
+            if (name === undefined){
+                message = `Les localités ont bien été trouvés`;
+            } else {
+                message = `La localité ${locality[0]["toponym"]} à bien été trouvée`;
+            }
         }
         res.json({message: message, body: locality})
 
     } catch (error) {
         res.status(404).json({error: error.message});
+    }
+});
+
+router.post('/', async (req, res) => {
+    try {
+        let message = "";
+        let locality = await localityService.setLocality({
+            name: req.body.name === undefined ? null : req.body.name,
+            postal_code: req.body.postal_code === undefined ? null : req.body.postal_code,
+            postal_code_complement: req.body.postal_code_complement === undefined ? null : req.body.postal_code_complement,
+            toponym: req.body.toponym === undefined ? null : req.body.toponym,
+            canton_code: req.body.canton_code === undefined ? null : req.body.canton_code,
+            language_code: req.body.language_code === undefined ? null : req.body.language_code
+        });
+
+        if (locality === null || locality === undefined) {
+            message = `Aucune localité n'a été ajoutée`;
+        } else {
+            message = `La localité ${req.body.name} à bien été ajoutée`;
+        }
+        res.status(404).json({message: message})
+
+    } catch (error) {
+        res.status(404).json({error: error.message});
+    }
+});
+
+router.patch('/:id/update', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+
+        if (await localityService.isIdValid(id)) {
+            let message = "";
+            let locality = await localityService.updateLocality({
+                id: id,
+                name: req.body.name === undefined ? null : req.body.name,
+                postal_code: req.body.postal_code === undefined ? null : req.body.postal_code,
+                postal_code_complement: req.body.postal_code_complement === undefined ? null : req.body.postal_code_complement,
+                toponym: req.body.toponym === undefined ? null : req.body.toponym,
+                canton_code: req.body.canton_code === undefined ? null : req.body.canton_code,
+                language_code: req.body.language_code === undefined ? null : req.body.language_code
+            });
+
+            if (locality < 1) {
+                message = `Erreur dans la mise à jour de la localité avec l'ID ${id}`;
+            } else {
+                message = `La localité ${req.body.name} à bien été modifiée`;
+            }
+            res.status(200).json({message: message})
+        } else {
+            const message = `La localité avec l'ID ${id} n'existe pas...`;
+            res.status(404).json({error: message})
+        }
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+
+        if (await localityService.isIdValid(id)) {
+            let message = "";
+            let locality = await localityService.deleteLocality(id);
+
+            if (locality < 1) {
+                message = `Erreur dans la suppression de la localité avec l'ID ${id}`;
+            } else {
+                message = `La localité avec l'ID ${id} à bien été supprimée`;
+            }
+            res.status(200).json({message: message})
+        } else {
+            const message = `La localité avec l'ID ${id} n'existe pas...`;
+            res.status(404).json({error: message})
+        }
+    } catch (error) {
+        res.status(500).json({error: error.message});
     }
 });
 
