d1813e0c8505d71bd088b48180f49e7efd9f9594
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2025-12-12 14:14:36 +0100

    feat:delete service route

--- routes/service.route.js
+++ routes/service.route.js
@@ -54,11 +54,30 @@ serviceRouter.patch('/:id/update', async (req, res) => {
         console.log("route.body", date, duration_m, location_id, dog_id) //success
         const resUpdateService = await serviceService.patchService(id, {date, duration_m, location_id, dog_id});
         if (resUpdateService === 0) {
-            res.status(404).json({error: "Activité non trouvée"})
+            res.status(404).json({error: "Service non trouvée"})
         } else {
             console.log("route.res", resUpdateService) //success
             let message = `Le service a bien été mis à jour !`;
             res.json({message: message, body: resUpdateService});
+        }
+
+    } catch (error) {
+        res.status(500).json({error: error.message});
+    }
+
+});
+
+serviceRouter.delete('/:id/delete', async (req, res) => {
+    try {
+        const id = parseInt(req.params.id)
+        console.log("route.id", id) //
+        const resDeleteService = await serviceService.deleteService(id);
+        if (resDeleteService === 0) {
+            res.status(404).json({error: "Service non trouvée"})
+        } else {
+            console.log("route.res", resDeleteService) //
+            let message = `Le service a bien été supprimé !`;
+            res.json({message: message, body: resDeleteService});
         }
 
     } catch (error) {
