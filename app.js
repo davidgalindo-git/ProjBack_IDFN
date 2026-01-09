529a19591e66a8db65d2dee9603083598360d889
davidgalindo-git <155019077+davidgalindo-git@users.noreply.github.com>
2026-01-09 12:04:55 +0100

    chore:update project name displayed in console

--- app.js
+++ app.js
@@ -34,5 +34,5 @@ app.use((req, res, next) => {
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`whatthedogs listening on port ${port}`)
 })
